6426bd2c2088a10d56b059a9a9e88395f404346f
sebastiano <sbonisolo@gmail.com>
2019-03-23 19:07:16 +0100

    Added domuino interface Updated domuino.hex Update logging for text box output

--- avrduino.py
+++ avrduino.py
@@ -3,11 +3,11 @@
 import subprocess
 import tkinter as tk
 import os
-from textwrap import wrap
+import logging
 
 from serial import rs485
 from simpledude import SimpleDude
-
+from domuino import Domuino, QUERIES
 
 BASEDIR = os.path.dirname(__file__)
 MAKEDIR = "/home/sebastiano/Documents/sloeber-workspace/optiboot485/optiboot/bootloaders/optiboot/"
@@ -28,15 +28,42 @@ BOOTLOADER = "optiboot_pro_8MHz.hex"
 DOMUINO = "domuino.hex"
 
 
+class TextHandler(logging.Handler):
+    # This class allows you to log to a Tkinter Text or ScrolledText widget
+    # Adapted from Moshe Kaplan: https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
+
+    def __init__(self, text):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Store a reference to the Text it will log to
+        self.text = text
+
+    def emit(self, record):
+        msg = self.format(record)
+
+        def append():
+            self.text.configure(state='normal')
+            self.text.insert(tk.END, msg + '\n')
+            self.text.configure(state='disabled')
+            # Autoscroll to the bottom
+            self.text.yview(tk.END)
+        # This is necessary because we can't modify the Text from other threads
+        self.text.after(0, append)
+
 
 class AvrDuino(object):
     def __init__(self, root):
-        self.txt_info = tk.Text(root)
+        self.txt_avr = tk.Text(root, width=50)
+        self.txt_domuino = tk.Text(root, width=50)
         btn_getinfo = tk.Button(root, text="AVR Info", width=25, command=self.get_info)
         btn_fuses = tk.Button(root, text="Set fuses", width=25, command=self.update_fuses)
         btn_bootloader = tk.Button(root, text="Flash Bootloader RS485", width=25, command=self.flash_bootloader)
-        btn_program = tk.Button(root, text="Upload Domuino", width=25, command=self.upload_domuino)
+        btn_program = tk.Button(root, text="Program Domuino", width=25, command=self.program_domuino)
+        btn_update = tk.Button(root, text="Update Domuino", width=25, command=self.update_domuino)
+        btn_start = tk.Button(root, text="Start Domuino", width=25, command=self.start_domuino)
         btn_stop = tk.Button(root, text="Cancel", width=25, command=root.destroy)
+        btn_clear_avr = tk.Button(root, text="Clear AVR", width=25, command=self.clear_avr)
+        btn_clear_dom = tk.Button(root, text="Clear Domuino", width=25, command=self.clear_dom)
         self.dry_run = tk.BooleanVar()
         chk_dry = tk.Checkbutton(root, text="Dry run", variable=self.dry_run)
 
@@ -53,20 +80,47 @@ class AvrDuino(object):
         btn_fuses.grid(row=1, columnspan=2, padx=5)
         btn_bootloader.grid(row=2, columnspan=2, padx=5)
         btn_program.grid(row=3, columnspan=2, padx=5)
-        btn_stop.grid(row=4, columnspan=2, padx=5)
-        label_id.grid(row=5, column=0, padx=5)
-        self.spinbox_id.grid(row=5, column=1, padx=5)
-        chk_dry.grid(row=6, columnspan=2, padx=5)
+        btn_update.grid(row=4, columnspan=2, padx=5)
+        btn_start.grid(row=5, columnspan=2, padx=5)
+        btn_stop.grid(row=6, columnspan=2, padx=5)
+        label_id.grid(row=7, column=0, padx=5)
+        self.spinbox_id.grid(row=7, column=1, padx=5)
+        chk_dry.grid(row=8, columnspan=2, padx=5)
 
-        self.txt_info.grid(row=0, column=2, rowspan=7, padx=5, pady=5)
-        self.txt_info.config(state=tk.DISABLED)
+        self.txt_avr.grid(row=0, column=2, rowspan=9, padx=5, pady=5)
+        btn_clear_avr.grid(row=9, column=2, padx=5, pady=5)
+        self.txt_domuino.grid(row=0, column=3, rowspan=9, padx=3, pady=5)
+        btn_clear_dom.grid(row=9, column=3, padx=5, pady=5)
 
-    def _show_info(self, infos):
-        self.txt_info.config(state=tk.NORMAL)
-        self.txt_info.delete("1.0", tk.END)
-        self.txt_info.insert(tk.END, infos)
-        self.txt_info.config(state=tk.DISABLED)
-        self.txt_info.pack()
+        self.ser = rs485.RS485('/dev/ttyUSB2', baudrate=38400, timeout=2)
+        n = int(self.number.get())
+        self.avr_handler = TextHandler(self.txt_avr)
+        self.logger = logging.getLogger(__name__)
+        self.logger.addHandler(self.avr_handler)
+
+        self.domuino = Domuino(1, self.ser)
+        self.domuino.hexfile = DOMUINO
+        self.dom_handler = TextHandler(self.txt_domuino)
+        self.domuino.log_handler(self.dom_handler)
+        self.domuino.daemon = True
+        self.domuino.start()
+
+    # def _show_info(self, infos):
+    #     self.avr_info.config(state=tk.NORMAL)
+    #     self.avr_info.delete("1.0", tk.END)
+    #     self.avr_info.insert(tk.END, infos)
+    #     self.avr_info.config(state=tk.DISABLED)
+    #     self.avr_info.pack()
+
+    def clear_avr(self):
+        self.txt_avr.config(state=tk.NORMAL)
+        self.txt_avr.delete("1.0", tk.END)
+        self.txt_avr.config(state=tk.DISABLED)
+
+    def clear_dom(self):
+        self.txt_domuino.config(state=tk.NORMAL)
+        self.txt_domuino.delete("1.0", tk.END)
+        self.txt_domuino.config(state=tk.DISABLED)
 
     def _run(self, command, io="stderr", work_dir=BASEDIR):
         run_ok = True
@@ -76,8 +130,6 @@ class AvrDuino(object):
                              stdin=subprocess.PIPE,
                              shell=True,
                              cwd=work_dir)
-        self.txt_info.config(state=tk.NORMAL)
-        self.txt_info.delete("1.0", tk.END)
         while p.returncode is None:
             if io == "stdout":
                 line = p.stdout.readline()
@@ -85,10 +137,7 @@ class AvrDuino(object):
                 line = p.stderr.readline()
             if any(sub in str(line) for sub in ["error:", "verification error"]):
                 run_ok = False
-            self.txt_info.config(state=tk.NORMAL)
-            self.txt_info.insert(tk.END, line)
-            self.txt_info.config(state=tk.DISABLED)
-            self.txt_info.see(tk.END)
+            self.logger.info(line)
             p.poll()
             root.update()
         return run_ok
@@ -115,12 +164,12 @@ class AvrDuino(object):
         full = self._run(cmd)
         errors = self._find_info(full, "error:")
         if errors:
-            self._show_info("\n".join(errors))
+            self.logger.error("\n".join(errors))
         else:
             info.extend(self._find_info(full, "Reading"))
             info.extend(self._find_info(full, "Device signature"))
             info.extend(self._find_info(full, "input file 0x"))
-            self._show_info("\n".join(info))
+            self.logger.info("\n".join(info))
 
     def _compile_bootloader(self):
         _id = int(self.spinbox_id.get())
@@ -130,28 +179,29 @@ class AvrDuino(object):
         cp = "cp {} {}".format(MAKEDIR+BOOTLOADER, BASEDIR)
         self._run("{}; {}".format(make, cp), io="stdout", work_dir=MAKEDIR)
 
+    def inc_number(self):
+        self.number.set(int(self.number.get()) + 1)
+        with open("backup.cfg", "w") as f:
+            f.write(self.number.get())
+        self.spinbox_id.update()
+
     def flash_bootloader(self):
         self._compile_bootloader()
 
-        if self._run(AVRCMD + "-u -U flash:w:\"{}\":i".format(BOOTLOADER)):
-            self.number.set(int(self.number.get()) + 1)
-            with open("backup.cfg", "w") as f:
-                f.write(self.number.get())
-            self.spinbox_id.update()
+        self._run(AVRCMD + "-u -U flash:w:\"{}\":i".format(BOOTLOADER))
 
-    def upload_domuino(self):
-        ser = rs485.RS485('/dev/ttyUSB2', baudrate=38400, timeout=2)
-        dude = SimpleDude(ser, hexfile= DOMUINO, mode485=True)
+    def start_domuino(self):
+        n = int(self.number.get())
 
-        self.txt_info.config(state=tk.NORMAL)
-        self.txt_info.delete("1.0", tk.END)
-        for progress in dude.program():
-            self.txt_info.config(state=tk.NORMAL)
-            self.txt_info.insert(tk.END, progress + "\n")
-            self.txt_info.config(state=tk.DISABLED)
-            self.txt_info.update()
-            self.txt_info.see(tk.END)
-        print("Program RS485")
+        self.domuino.send(n, bytearray((QUERIES["RUN"], )))
+
+    def update_domuino(self):
+        n = int(self.number.get())
+        self.domuino.send(n, bytearray((QUERIES["RESET"], )))
+
+    def program_domuino(self):
+        dude = SimpleDude(self.ser, hexfile=DOMUINO, mode485=True)
+        dude.program()
 
 
 if __name__ == '__main__':
--- simpledude.py
+++ simpledude.py
@@ -68,7 +68,6 @@ GET_SIGNATURE = [STK_READ_SIGN, CRC_EOP]
 INSINK = [STK_INSYNC, STK_OK]
 
 logging.basicConfig(format='%(message)s', level=logging.INFO)
-_LOGGER = logging.getLogger(__name__)
 
 
 class SimpleDude(object):
@@ -77,13 +76,17 @@ class SimpleDude(object):
         self.hexfile = hexfile
         self.retry = retry
         self.mode485 = mode485
+        self.logger = logging.getLogger(__name__)
+
+    def log_handler(self, handler):
+        self.logger.addHandler(handler)
 
     def spi_transaction(self, codes, bytesreply=0):
         n = 0
         retry = True
         tx_complete = len(codes) * 0.53 + 1
         while retry:
-            _LOGGER.debug("Send %s", [hex(b) for b in codes])
+            self.logger.debug("Send %s", [hex(b) for b in codes])
             time.sleep(0.01)
             # self.sock.setRTS(False)
             tx_start = time.time()
@@ -91,18 +94,18 @@ class SimpleDude(object):
             while (time.time() - tx_start) < (tx_complete / 1000.0):
                 pass
             # self.sock.setRTS(True)
-            _LOGGER.debug("Wait for reply")
+            self.logger.debug("Wait for reply")
             # Wait for bytesreply + INSYNC + OK
             reply = list(self.sock.read(size=bytesreply + 2))
-            _LOGGER.debug("Received %s", [hex(b) for b in reply])
+            self.logger.debug("Received %s", [hex(b) for b in reply])
             if not reply or ([reply[0], reply[-1]] != INSINK):
                 if n < self.retry:
                     n += 1
-                    _LOGGER.critical("Retry %s", n)
+                    self.logger.critical("Retry %s", n)
                     continue
                 else:
-                    _LOGGER.critical("Not in sync")
-                    raise BaseException("Not in sync")
+                    self.logger.critical("Not in sync")
+                    raise Exception("Not in sync")
             if len(reply) == 3:
                 return reply[1]
             elif len(reply) > 3:
@@ -112,65 +115,65 @@ class SimpleDude(object):
     def sync(self):
         # get in self.sync with the AVR
         for i in range(3):
-            _LOGGER.debug("Syncing")
+            self.logger.debug("Syncing")
             self.spi_transaction(SYNC)
 
     def get_info(self):
         self.sync()
     
         # get the MAJOR version of the bootloader
-        _LOGGER.debug("Getting the HARDWARE version of arduino")
+        self.logger.debug("Getting the HARDWARE version of arduino")
         hardware = self.spi_transaction(GET_HARDWARE, 1)
     
-        _LOGGER.info("Hardware version: %s", hex(hardware))
+        self.logger.info("Hardware version: %s", hex(hardware))
     
         # get the MAJOR version of the bootloader
-        _LOGGER.debug("Getting the MAJOR version of the bootloader")
+        self.logger.debug("Getting the MAJOR version of the bootloader")
         major = self.spi_transaction(GET_SW_MAJOR, 1)
     
         # get the MINOR version of the bootloader
-        _LOGGER.debug("Getting the MINOR version of the bootloader")
+        self.logger.debug("Getting the MINOR version of the bootloader")
         minor = self.spi_transaction(GET_SW_MINOR, 1)
     
-        _LOGGER.info("Bootloader version %s.%s", hex(major), hex(minor))
+        self.logger.info("Bootloader version %s.%s", hex(major), hex(minor))
     
-        _LOGGER.debug("Getting the MAJOR serial number of arduino")
+        self.logger.debug("Getting the MAJOR serial number of arduino")
         major = self.spi_transaction(GET_SN_MAJOR, 1)
 
-        _LOGGER.debug("Getting the MINOR serial number of arduino")
+        self.logger.debug("Getting the MINOR serial number of arduino")
         minor = self.spi_transaction(GET_SN_MINOR, 1)
 
-        _LOGGER.info("Serial number %s", major*256 + minor)
+        self.logger.info("Serial number %s", major*256 + minor)
 
         # enter programming mode
-        _LOGGER.debug("Entering programming mode")
+        self.logger.debug("Entering programming mode")
         self.spi_transaction(ENTER_PROG_MODE)
     
         # get device signature
-        _LOGGER.debug("Getting device signature")
+        self.logger.debug("Getting device signature")
         signature = self.spi_transaction(GET_SIGNATURE, 3)
     
-        _LOGGER.info("Device signature %s-%s-%s", hex(signature[0]), hex(signature[1]), hex(signature[2]))
+        self.logger.info("Device signature %s-%s-%s", hex(signature[0]), hex(signature[1]), hex(signature[2]))
     
-        _LOGGER.debug("Get safe fuses")
+        self.logger.debug("Get safe fuses")
         lfuse = self.spi_transaction(GET_SAFE_LFUSE, 1)
         hfuse = self.spi_transaction(GET_SAFE_HFUSE, 1)
         efuse = self.spi_transaction(GET_SAFE_EFUSE, 1)
     
-        _LOGGER.info("FUSES E:%s H:%s L:%s", hex(efuse), hex(hfuse), hex(lfuse))
+        self.logger.info("FUSES E:%s H:%s L:%s", hex(efuse), hex(hfuse), hex(lfuse))
     
         # leave programming mode
-        _LOGGER.debug("Leaving programming mode")
+        self.logger.debug("Leaving programming mode")
         self.spi_transaction(EXIT_PROG_MODE)
     
     def program(self):
         self.sync()
         # enter programming mode
-        # _LOGGER.info("Chip erase")
+        # self.logger.info("Chip erase")
         # self.spi_transaction(CHIP_ERASE)
 
         # enter programming mode
-        _LOGGER.debug("Entering programming mode")
+        self.logger.debug("Entering programming mode")
         self.spi_transaction(ENTER_PROG_MODE)
 
         # start with page address 0
@@ -191,15 +194,15 @@ class SimpleDude(object):
                 if row[7:9] != b'01':
                     hexrow = row[9:][:-4]
                     data.extend([int(hexrow[b:b + 2], 16) for b in range(len(hexrow))[::2]])
-                    _LOGGER.debug("ROW: %s - len data: %s", row, len(data))
+                    self.logger.debug("ROW: %s - len data: %s", row, len(data))
                 if not data:
-                    _LOGGER.debug("End program")
+                    self.logger.debug("End program")
                     break
                 if len(data) >= 128 or row[7:9] == b'01':
                     size = len(data[:128])
                     prg_length += size
                     # load page address
-                    _LOGGER.debug("Sending page address")
+                    self.logger.debug("Sending page address")
 
                     # calculate page address
                     laddress = address % 256
@@ -207,21 +210,20 @@ class SimpleDude(object):
                     address += 64
 
                     self.spi_transaction([STK_LOAD_ADDRESS, laddress, haddress, CRC_EOP])
-                    msg = "Sending page {}:{} block size:{}".format(haddress, laddress, size)
-                    yield msg
-                    _LOGGER.info(msg + "data:{}".format(list(map(hex, data[:128]))))
+                    self.logger.info("Sending page {}:{} block size:{}".format(haddress, laddress, size))
+                    self.logger.debug("Data:{}".format(list(map(hex, data[:128]))))
                     self.spi_transaction([STK_PROG_PAGE, 0, size, FLASH_MEMORY] + data[:128] + [CRC_EOP])
                     data = data[128:]
 
         # leave programming mode
-        _LOGGER.debug("Leaving programming mode")
+        self.logger.debug("Leaving programming mode")
         self.spi_transaction(EXIT_PROG_MODE)
-        _LOGGER.info("Program size %s bytes", prg_length)
+        self.logger.info("Program size %s bytes", prg_length)
 
     def verify(self):
         self.sync()
         # enter programming mode
-        _LOGGER.debug("Entering programming mode")
+        self.logger.debug("Entering programming mode")
         self.spi_transaction(ENTER_PROG_MODE)
     
         # start with page address 0
@@ -237,7 +239,7 @@ class SimpleDude(object):
                 address += 64
     
                 # load page address
-                _LOGGER.debug("Sending page address")
+                self.logger.debug("Sending page address")
                 self.spi_transaction([STK_LOAD_ADDRESS, laddress, haddress, CRC_EOP])
     
                 data = list()
@@ -251,21 +253,21 @@ class SimpleDude(object):
     
                 size = len(data)
                 # half the size
-                _LOGGER.info("Reading program page %s:%s", haddress, laddress)
+                self.logger.info("Reading program page %s:%s", haddress, laddress)
                 page = self.spi_transaction([STK_READ_PAGE, 0, 0x80, FLASH_MEMORY, CRC_EOP], 0x80)
     
                 if data != page[:len(data)]:
-                    _LOGGER.error("Error! Page %s:%s differ from Hex file", haddress, laddress)
+                    self.logger.error("Error! Page %s:%s differ from Hex file", haddress, laddress)
                     # leave programming mode
-                    _LOGGER.debug("Leaving programming mode")
+                    self.logger.debug("Leaving programming mode")
                     self.spi_transaction(EXIT_PROG_MODE)
                     return False
 
                 # FIX: Allineare con program
                 if size != 0x80:
-                    _LOGGER.info("Program check OK.")
+                    self.logger.info("Program check OK.")
                     # leave programming mode
-                    _LOGGER.debug("Leaving programming mode")
+                    self.logger.debug("Leaving programming mode")
                     self.spi_transaction(EXIT_PROG_MODE)
                     return True
 
@@ -277,6 +279,6 @@ if __name__ == '__main__':
     # dude = SimpleDude(ser, hexfile="/home/sebastiano/Documents/sloeber-workspace/blink/Release/blink.hex", mode485=True)
     dude = SimpleDude(ser, hexfile="domuino.hex", mode485=True)
     # dude = SimpleDude(ser, hexfile="/home/sebastiano/Documents/sloeber-workspace/testssd1306ascii/Release/testssd1306ascii.hex", mode485=True)
-    #dude.get_info()
+    # dude.get_info()
     dude.program()
-    # dude.verify()
+    #dude.verify()
